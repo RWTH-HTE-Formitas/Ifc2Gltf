d6ad84941d2fa6ca3af5076fb25e58d49bdb600b
Monika Kovac <monika.kovac@rwth-aachen.de>
2018-11-12 16:22:41 +0100

    Added ConerterTest

--- test/Unit/ConverterTest.php
+++ test/Unit/ConverterTest.php
@@ -2,12 +2,52 @@
 
 namespace Ifc2Gltf\Test\Unit;
 
+use Ifc2Gltf\Converter;
 use PHPUnit\Framework\TestCase;
 
 final class ConverterTest extends TestCase
 {
-    public function test()
+
+    public function testIfEndFileExists ()
     {
-        static::assertTrue(true);
+        $file = (new Converter())->convertIfcToGlTF('../assets/Office_A_20110811.ifc');
+        static::assertTrue(file_exists($file));
+    }
+
+    public function testEndFileExtension ()
+    {
+        $file = (new Converter())->convertIfcToGlTF('../assets/Office_A_20110811.ifc');
+        $info = pathinfo($file);
+        static::assertEquals('.gltf', $info['extension']);
+    }
+
+
+
+    public function testIfColladaFileExists ()
+    {
+        $file = (new Converter())->convertIfcToCollada('../assets/Office_A_20110811.ifc');
+        static::assertTrue(file_exists($file));
+    }
+
+    public function testColladaFileExtension ()
+    {
+        $file = (new Converter())->convertIfcToCollada('../assets/Office_A_20110811.ifc');
+        $info = pathinfo($file);
+        static::assertEquals('.dae', $info['extension']);
+    }
+
+
+
+    public function testIfGltfFileExists ()
+    {
+        $file = (new Converter())->convertColladaToGlTF('../assets/Office_A_20110811.ifc');
+        static::assertTrue(file_exists($file));
+    }
+
+    public function testGltfFileExtension ()
+    {
+        $file = (new Converter())->convertColladaToGlTF('../assets/Office_A_20110811.ifc');
+        $info = pathinfo($file);
+        static::assertEquals('.gltf', $info['extension']);
     }
 }
